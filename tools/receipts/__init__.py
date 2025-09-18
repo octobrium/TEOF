9247a3c0915580405ca958700b94458fa2156523
Octo <36472476+octobrium@users.noreply.github.com>
2025-09-18 16:36:30 -0700

    Restore queued status for new apoptosis plans

--- tools/receipts/__init__.py
+++ tools/receipts/__init__.py
@@ -1,5 +1,3 @@
-"""Receipt tooling helpers."""
+"""Receipts tooling package."""
 
-from .scaffold import ScaffoldResult, scaffold_claim, scaffold_plan
-
-__all__ = ["ScaffoldResult", "scaffold_plan", "scaffold_claim"]
+__all__: list[str] = []
